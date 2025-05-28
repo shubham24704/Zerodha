fd0e398d3e3b3888daea5e4e851aafe61f8be929
Shubham Shukla <shuklashubham24704@gmail.com>
2025-05-28 17:45:00 +0530

    Finished Product section

--- src/landing_page/product/RightSection.js
+++ src/landing_page/product/RightSection.js
@@ -1,9 +1,25 @@
 import React from 'react';
 
-function RightSection() {
-    return ( 
-        <h1>RightSection</h1>
-     );
+function RightSection({ imageURL, productName, productDesription, learnMore }) {
+  return (
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-5 p-5 mt-5">
+          <h1 className='p-3'>{productName}</h1>
+          <p className='p-3 text-muted'>{productDesription}</p>
+          <div className='p-3'>
+            <a href={learnMore}>Learn More <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+            
+          </div>
+        </div>
+        <div className="col-1"></div>
+        <div className="col-6">
+          <img src={imageURL} alt='Loading...'/>
+        </div>
+      </div>
+    </div>
+  );
 }
+
 
 export default RightSection;
--- src/landing_page/product/Universe.js
+++ src/landing_page/product/Universe.js
@@ -1,9 +1,67 @@
-import React from 'react';
+import React from "react";
 
 function Universe() {
-    return ( 
-        <h1>Universe</h1>
-     );
+  return (
+    <div className="container mt-5">
+      <div className="row text-center">
+        <h1>The Zerodha Universe</h1>
+        <p>
+          Extend your trading and investment experience even further with our
+          partner platforms
+        </p>
+
+        <div className="col-4 p-3 mt-5">
+          <img src="media/smallcaseLogo.png" />
+          <p className="text-small text-muted p-3" style={{fontSize:"15px"}}>
+            Our asset management venture that is creating simple and transparent
+            index funds to help you save for your goals.
+          </p>
+        </div>
+        <div className="col-4 p-3 mt-5">
+          <img src="media/smallcaseLogo.png" />
+          <p className="text-small text-muted p-3" style={{fontSize:"15px"}}>
+            Options trading platform that lets you create strategies, analyze
+            positions, and examine data points like open interest, FII/DII, and
+            more.
+          </p>
+        </div>
+        <div className="col-4 p-3 mt-5">
+          <img src="media/smallcaseLogo.png" />
+          <p className="text-small text-muted p-3" style={{fontSize:"15px"}}>
+            Investment research platform that offers detailed insights on
+            stocks, sectors, supply chains, and more.
+          </p>
+        </div>
+        <div className="col-4 p-3 mt-5">
+          <img src="media/smallcaseLogo.png" />
+          <p className="text-small text-muted p-3" style={{fontSize:"15px"}}>
+            Systematic trading platform that allows you to create and backtest
+            strategies without coding.
+          </p>
+        </div>
+        <div className="col-4 p-3 mt-5">
+          <img src="media/smallcaseLogo.png" />
+          <p className="text-small text-muted p-3" style={{fontSize:"15px"}}>
+            Thematic investing platform that helps you invest in diversified
+            baskets of stocks on ETFs.
+          </p>
+        </div>
+        <div className="col-4 p-3 mt-5">
+          <img src="media/smallcaseLogo.png" />
+          <p className="text-muted p-3" style={{fontSize:"15px"}}>
+            Personalized advice on life and health insurance. No spam and no
+            mis-selling.
+          </p>
+        </div>
+        <button
+          className="p-2 btn btn-primary fs-5 mb-5"
+          style={{ width: "20%", margin: "0 auto" }}
+        >
+          Signup Now
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Universe;
